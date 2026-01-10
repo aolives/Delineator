93326104d8343cb55a1a601fde409d670b9c4271
Adrian Big Challenges Olives <AdrianOlives975@gmail.com>
2026-01-09 18:40:53 -0800

    PHP 8.4->8.5

--- public/index.php
+++ public/index.php
@@ -4,4 +4,7 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-return fn(array $context) => new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+return fn(array $context) => new Kernel(
+    is_string($context['APP_ENV']) ? $context['APP_ENV'] : 'prod',
+    (bool) $context['APP_DEBUG']
+);
